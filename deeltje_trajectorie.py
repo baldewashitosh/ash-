cd1ace6f92d76b529f2955c23399eeacd9e8ab90
Baldew Ashitosh <ashitoshbaldew18@gmail.com>
2023-11-22 12:17:43 -0800

    Eerste commit

--- deeltje_trajectorie.py
+++ deeltje_trajectorie.py
@@ -1,3 +1,12 @@
+"""
+Name:baldew ashitosh
+
+Date:
+
+Porgram description:
+"""
+
 def deeltje_trajectorie(t, zwaartekracht,beginsnelheid, beginsnelheid)
     positie = -0.5 * zwaartekracht * t**2 + beginsnelheid *t
     return positie
+
